1b74669e0dc57ea4cedf6c6908d5d607b90c8101
Shayane Tan <shayane_tan@yahoo.com>
2017-04-16 20:07:43 +0800

    Added main menu and control guide scenes

---
+++ New Unity Project 2D Movement Action/Assets/scripts/SceneManagement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour {
+
+	public void ToCharacterSelect(){
+		SceneManager.LoadScene("Character_Select");
+	}
+	
+	public void ToControlGuide(){
+		SceneManager.LoadScene("Control_Guide");
+	}
+	
+	public void ToMainMenu(){
+		SceneManager.LoadScene("Main_Menu");
+	}
+}
